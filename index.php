06238bc6aa255322287832a8277d3699e72e82ca
RafaelFabro <78381654+RafaelFabro@users.noreply.github.com>
2021-02-02 15:23:03 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -73,13 +73,13 @@
             <div class="card-body d-flex flex-column align-items-start">
               <strong class="d-inline-block mb-2 text-primary">World</strong>
               <h3 class="mb-0">
-                <a class="text-dark" href="#">Featured post</a>
+                <a class="text-dark" href="#">Frango virtual</a>
               </h3>
-              <div class="mb-1 text-muted">Nov 12</div>
-              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#">Continue reading</a>
+              <div class="mb-1 text-muted">R$75,00</div>
+              <p class="card-text mb-auto">Frango virtual para sua diversão.</p>
+              <a href="#">Compre agora</a>
             </div>
-            <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
+            <img class="card-img-right flex-auto d-none d-md-block" data-src="/Frango Virtual.jpg" alt="Card image cap">
           </div>
         </div>
         <div class="col-md-6">
